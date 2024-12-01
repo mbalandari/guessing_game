41640faeacaadef597d1b6f8ff625c9ca5f5da78
codingMaz <m.balandari.toroghi@gmail.com>
2024-12-01 17:40:33 +0330

    create the random number for guessing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use rand::Rng;
+use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 }
